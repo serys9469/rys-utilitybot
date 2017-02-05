8c85cf153fa9169b7d14c1279413684007868411
serys9469 <serys9469.japan@gmail.com>
2017-02-05 16:13:08 +0900

    Update SearchManager.java

--- UtilityBot-Line/src/main/java/com/github/kiyohitonara/digestbot/SearchManager.java
+++ UtilityBot-Line/src/main/java/com/github/kiyohitonara/digestbot/SearchManager.java
@@ -90,16 +90,16 @@ public class SearchManager {
                     StringBuilder stringBuilder = new StringBuilder();
 
                     for (DatabaseRecord previousDatabaseRecord : databaseManager.queryOfPreviousById(databaseRecord.getId(), groupId, 2)) {
-                        stringBuilder.append( previousDatabaseRecord.getDate() + " ~\n" + "---------------------\n");
+                        stringBuilder.append( previousDatabaseRecord.getDate() + " ~\n---------------------\n" );
                         break;
                     }
                     
                     for (DatabaseRecord previousDatabaseRecord : databaseManager.queryOfPreviousById(databaseRecord.getId(), groupId, 2)) {
-                        stringBuilder.append( previousDatabaseRecord.getText() + "\n");
+                        stringBuilder.append( previousDatabaseRecord.getText() + "\n---------------------\n");
                     }
                     stringBuilder.append( databaseRecord.getText() + "\n" + "---------------------\n");
                     for (DatabaseRecord afterDatabaseRecord : databaseManager.queryOfAfterById(databaseRecord.getId(), groupId, 2)) {
-                        stringBuilder.append( afterDatabaseRecord.getText() + "\n" + "---------------------");
+                        stringBuilder.append( afterDatabaseRecord.getText() + "\n");
                     }
 
                     messages.add(new TextMessage(stringBuilder.toString()));
